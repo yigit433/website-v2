74db9d976d34126be1db6fabdc72e088fb75510c
Yiğit <yigit433.devs@gmail.com>
2025-06-10 06:04:00 +0300

    Update Hero component for improved responsiveness
    - Modified class names in the Hero component to utilize Tailwind CSS responsive utilities, enhancing layout behavior on smaller screens.

--- components/Home/Hero.tsx
+++ components/Home/Hero.tsx
@@ -4,7 +4,7 @@ import HeroImage from "@/components/Home/HeroImage";
 
 export default function Hero() {
     return (
-        <div className="mt-4 flex non910:flex-col-reverse justify-around items-center">
+        <div className="mt-4 flex max-[940px]:flex-col-reverse justify-around items-center">
             <div className="max-w-2xl flex flex-col lg:text-left text-center">
                 <h1 className="font-semibold text-3xl md:text-4xl text-alignment">
                     Self{" "}
