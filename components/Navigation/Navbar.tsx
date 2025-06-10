bd560693703f58fb61c919038f0133a9ac802488
Yiğit <yigit433.devs@gmail.com>
2025-06-10 06:18:59 +0300

    Comment out unused import in Navbar component
    - Removed the useState import from the Navbar component to streamline the code and reduce unnecessary dependencies.
    - This change aligns with previous efforts to enhance code clarity and prepare for future updates.

--- components/Navigation/Navbar.tsx
+++ components/Navigation/Navbar.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState } from "react";
+// import { useState } from "react";
 import { usePathname, useRouter } from "next/navigation";
 // import { AnimatePresence } from "framer-motion";
 
